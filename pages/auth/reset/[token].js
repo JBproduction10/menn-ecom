eb0fe995b7fc0a1ab17a287f1b38bdb921a8aa3a
Jonathan Bangala <jbangala90@gmail.com>
2023-12-23 10:16:19 +0200

    working authentication send email verification

--- pages/auth/reset/[token].js
+++ pages/auth/reset/[token].js
@@ -1,9 +1,108 @@
-import React from 'react'
+import React from 'react';
+import styles from "../../../styles/forgot.module.scss";
+import Header from "../../../components/header";
+import Footer from "../../../components/footer";
+import { BiLeftArrowAlt } from "react-icons/bi";
+import CircledIconBtn from "../../../components/buttons/circledIconBtn";
+import LoginInput from "../../../components/inputs/loginInput";
+import { useState } from "react";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import Link from "next/link";
+import DotLoaderSpinner from "../../../components/loaders/dotLoader";
+import axios from "axios";
+import { getSession, signIn } from "next-auth/react";
+import jwt from "jsonwebtoken";
+import { Router } from "next/router";
 
-const index = () => {
-  return (
-    <div>index</div>
+export default function reset({user_id}){
+  const [password, setPassword] = useState("");
+  const [conf_password, setConf_password] = useState("");
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState('');
+  const [loading, setLoading] = useState("");
+  const passwordValidation = Yup.object({
+    password: Yup.string()
+      .required("Please enter your new password.")
+      .min(6, "password must be at least 6 characters.")
+      .max(36, "Password can't be more than 36 characters"),
+    conf_password: Yup.string()
+      .required("Confirm your password.")
+      .oneOf([Yup.ref("password")], "Passwords must match."),
+  });
+  const resetHandler = async() =>{
+    try{
+      setLoading(true);
+      const {data} = await axios.put("/api/auth/reset", {
+        user_id,
+        password,
+      });
+      let options ={
+        redirect: false,
+        email: data.email,
+        password: password,
+      };
+      await signIn("credentials", options);
+      window.location.reload(true);
+    }catch(error){
+      setLoading(false);
+      setSuccess("");
+      setError(error.response.data.message);
+    }
+  };
+
+  return(
+    <>
+      {loading && <DotLoaderSpinner loading={loading}/>}
+      <Header country=''/>
+      <div className={styles.forgot}>
+        <div>
+          <div className={styles.forgot__header}>
+            <div className={styles.back__svg}>
+              <BiLeftArrowAlt/>
+            </div>
+            <span>
+              Reset your password? <Link href='/'>Login instead</Link>
+            </span>
+          </div>
+          <Formik
+            enableReinitialize
+            initialValues={{
+              password,
+              conf_password
+            }}
+            validationSchema={passwordValidation}
+            onSubmit={() =>{
+              resetHandler()
+            }}
+          >
+            {(form) =>(
+              <Form>
+                <LoginInput
+                  type='password'
+                  name='password'
+                  icon='password'
+                  placeholder='Password'
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <LoginInput
+                  type='password'
+                  name='conf_password'
+                  icon='password'
+                  placeholder='Confirm Password'
+                  onChange={(e) => setConf_password(e.target.value)}
+                />
+
+                <CircledIconBtn type='submit' text='submit'/>
+                <div style={{marginTop: '20px'}}>
+                  {error && <span className={styles.error}>{error}</span>}
+                </div>
+              </Form>
+            )}
+          </Formik>
+        </div>
+      </div>
+      <Footer country=''/>
+    </>
   )
 }
-
-export default index
